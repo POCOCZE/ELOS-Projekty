a392674f3e6fcdf33056d634d3543c0405ce66f6
POCOCZE <simon.pradka@gmail.com>
2024-08-15 10:34:11 +0000

    Added beautuful comments
    Added more comments, sorted them so sections for better navigation of the code.

--- Bash/create-users.sh
+++ Bash/create-users.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
-# Creates N number of users with the same password for all of them
+# Creates N number of users with same password for all of them
 # Directory .ssh will be created for all of the users with public/private keys
 # Created by simon
-# Date 2024/8/14 - Y/M/D
+# Date 2024/8/8 - Y/M/D
 
-# Variables to change:
+# Lower cammel case is used for changing variables to store data for a limited time
+# E.g.: username
+# Capital case is used to Constants and env variables
+# E.g.: USERNAME, PASSWD, ...
+
+##-----------##
+## Variables ##
+##-----------##
+
 LOGFILE=/var/log/log.txt
 USERNAME=tom
 PASSWD=student
 PERMISSIONLEVEL=600
 
-echo "--- CREATE MULTIPLE USERS ---"
-echo
+##-----------##
+## Functions ##
+##-----------##
 
-# Function to log messages
+# Log message into file
 log_message() {
     local message="$1"
     echo "$message" >> $LOGFILE
 }
 
-# Function to create a new user
+# Create new user
 create_user() {
     local username="$1"
     if useradd -m $username && echo "$username:$PASSWD" | chpasswd; then
@@ -28,10 +37,11 @@ create_user() {
         log_message "[ERROR] Unable to create user and password"
         exit 1
     fi
-}
+}    
 
-# Function to create .ssh directory and generate keys
-create_ssh_keys() {
+# Create .ssh dir and generate there keys
+# dir /dev/null is a special file that discards any input
+create_ssh_dir_and_keys() {
     local username="$1"
     if mkdir -p /home/$username/.ssh && ssh-keygen -t rsa -N "" -f /home/$username/.ssh/sshkey > /dev/null; then
         log_message "[INFO] Created directory .ssh and generated keys"
@@ -41,8 +51,8 @@ create_ssh_keys() {
     fi
 }
 
-# Function to set permissions for private key
-set_permissions() {
+# Permissions for private key
+set_permissions_priv_key() {
     local username="$1"
     if chmod $PERMISSIONLEVEL /home/$username/.ssh/sshkey; then
         log_message "[INFO] Set permissions for private key file"
@@ -52,50 +62,59 @@ set_permissions() {
     fi
 }
 
-# Function to change ownership of the .ssh folder
-change_ownership() {
+# Change the ownership of the .ssh folder to $USERNAME
+change_ssh_dir_owner() {
     local username="$1"
     if sudo chown -R $username:$username /home/$username/.ssh; then
-        log_message "[INFO] Changed ownership of the .ssh folder"
+        log_message "[INFO] Changed ownership of the .ssh forder"
     else
-        log_message "[ERROR] Unable to change ownership of the .ssh folder"
+        log_message "[ERROR] Unable to change ownership of the .ssh forder"
         exit 1
     fi
 }
 
-# Infinite loop
+##------##
+## Main ##
+##------##
+
+# Create a log file, write info and create basic structure
+touch $LOGFILE
+log_message "# ------------------------------------------------"
+log_message "Date: $(date)"
+log_message ""
+
+# Write to user
+echo "--- CREATE MULTIPLE USERS ---"
+echo
+
 while : ; do
+	echo "Enter number of users to create:"
+	read NUMUSERS
 
-    echo "Enter number of users to create:"
-    read NUMUSERS
+	# Check whether the input number is positive
+	# Implemented regex - regular expression - checks whether input read number is positive integer
+	# e.g.: Numbers like 1.5, 01, abc will make an error message to appear on screen
+	if [[ ! "$NUMUSERS" =~ ^[1-9][0-9]*$ ]]; then
+		echo "[ERROR] Input number is not a positive integer!"
+        echo
+        echo
+        # Skip current iteration and move the next
+		continue
+	fi
 
-    # Create a log file, write info and create basic structure
-    touch $LOGFILE
-    log_message "# ------------------------------------------------"
-    log_message "Date: $(date)"
-    log_message ""
-
-    # Check whether the input number is positive
-    if [[ ! "$NUMUSERS" =~ ^[1-9][0-9]*$ ]]; then
-        echo "[ERROR] Input number is not a positive integer!"
-        continue
-    fi
-
-    # Loop to iterate over the number of users set by user
-    for ((i=1; i<=NUMUSERS; i++)); do
-
-        username="$USERNAME$i"
+	# Loop to iterate over the number of users set by user
+    # Dolar sign can be ommited because of aritmetic context in the condition
+	for ((i=1; i<=NUMUSERS; i++)); do
+        username=$USERNAME$i
         log_message "[INFO] ${i} User: $username"
 
         create_user $username
-        create_ssh_keys $username
-        set_permissions $username
-        change_ownership $username
-
-    done
-
-    # End the while loop and jump out
-    break
+        create_ssh_dir_and_keys $username
+        set_permissions_priv_key $username
+        change_ssh_dir_owner $username
+	done
+	# End the while loop and jump out
+	break
 done
 
 echo "Successfully created $NUMUSERS users."
