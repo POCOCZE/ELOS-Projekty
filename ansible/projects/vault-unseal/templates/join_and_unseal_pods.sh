f1b4503dafdc170669b34fafd9c2f06167acc397
POCOCZE <simon.pradka@gmail.com>
2024-11-19 18:12:45 +0100

    Fixed Unsealing Vault
    Fixed Unsealing HashiCorp Vault. Problem was in unsufficient number of Unseal Keys parsed (default 3), changed to 5. Tried manually parsing the 3 Unseal Keys, not worked. If done with 4 and more, then unseal was done correctly and Pod was connected with other peers.

--- ansible/projects/vault-unseal/templates/join_and_unseal_pods.sh
+++ ansible/projects/vault-unseal/templates/join_and_unseal_pods.sh
@@ -9,13 +9,13 @@
 NAMESPACE=$1
 VAULT_POD_0=$2
 REPLICA_COUNT=$3
-LEADER_CA_CERT=$4
+# LEADER_CA_CERT=$4
 
 # Function to Join and Unseal a Pod
 join_and_unseal_pod() {
   local pod_name=$1
-  local join_command="vault operator raft join -leader-ca-cert=@${LEADER_CA_CERT} https://${VAULT_POD_0}.vault-internal:8200"
-  local unseal_keys=$(head -n 3 /tmp/unseal_keys.txt)
+  local join_command="vault operator raft join -leader-ca-cert=@/vault/userconfig/vault-ha-tls/ca.crt https://${VAULT_POD_0}.vault-internal:8200" #-leader-ca-cert=@${LEADER_CA_CERT}
+  local unseal_keys=$(head -n 5 /tmp/unseal_keys.txt)
 
   # Join the pod to the Raft
   kubectl exec -n ${NAMESPACE} pod/${pod_name} -- ${join_command}
